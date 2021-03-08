b447672b0bc3bce084d4b9c8374add4a84aa1271
Bread <osu@kakao.com>
2021-03-08 13:20:26 +0900

    🎨 Add try...catch block

--- commands/music/leave.js
+++ commands/music/leave.js
@@ -5,30 +5,34 @@ module.exports = {
     cooldown: "5",
 
     run: async (client, message) => {
-        if (!message.member.voice.channel) return message.channel.send("보이스채널에 먼저 들어가셔야 해요.")
-        if (client.distube.isPlaying(message)) {
-            const filter = m => m.author.id === message.author.id
-            return message.channel.send("재생중인 노래(또는 대기열)이 있습니다, 노래를 정지시킬까요? **(네 / 아니요)**").then(() => {
-                message.channel.awaitMessages(filter, {
-                    max: 1,
-                    time: 30000,
-                    errors: ["time"]
+        try {
+            if (!message.member.voice.channel) return message.channel.send("보이스채널에 먼저 들어가셔야 해요.")
+            if (client.distube.isPlaying(message)) {
+                const filter = m => m.author.id === message.author.id
+                return message.channel.send("재생중인 노래(또는 대기열)이 있습니다, 노래를 정지시킬까요? **(네 / 아니요)**").then(() => {
+                    message.channel.awaitMessages(filter, {
+                        max: 1,
+                        time: 30000,
+                        errors: ["time"]
+                    })
+                        .then(message => {
+                            message = message.first()
+                            if (message.content.toUpperCase() === "네") {
+                                client.distube.stop(message)
+                            } else if (message.content.toUpperCase() === "아니요") {
+                                message.channel.send("계속 노래를 틀을게요.")
+                            } else {
+                                message.channel.send("알맞지 않은 대답이에요.")
+                            }
+                        })
+                        .catch(collected => {
+                            message.channel.send("30초가 지났어요.")
+                        })
                 })
-                    .then(message => {
-                        message = message.first()
-                        if (message.content.toUpperCase() === "네") {
-                            client.distube.stop(message)
-                        } else if (message.content.toUpperCase() === "아니요") {
-                            message.channel.send("계속 노래를 틀을게요.")
-                        } else {
-                            message.channel.send("알맞지 않은 대답이에요.")
-                        }
-                    })
-                    .catch(collected => {
-                        message.channel.send("30초가 지났어요.")
-                    })
-            })
+            }
+            message.guild.voice.channel.leave()
+        } catch (e) {
+            message.channel.send(`에러가 발생했습니다.\n\`${e}\``)
         }
-        message.guild.voice.channel.leave()
     }
 }
--- commands/music/skip.js
+++ commands/music/skip.js
@@ -5,8 +5,12 @@ module.exports = {
     cooldown: "5",
 
     run: async (client, message) => {
-        if (!message.member.voice.channel) return message.channel.send("보이스채널에 먼저 들어가셔야 해요.")
-        if (!client.distube.isPlaying(message)) return message.channel.send("듣고 계신거 맞죠?!")
-        client.distube.skip(message)
+        try {
+            if (!message.member.voice.channel) return message.channel.send("보이스채널에 먼저 들어가셔야 해요.")
+            if (!client.distube.isPlaying(message)) return message.channel.send("듣고 계신거 맞죠?!")
+            client.distube.skip(message)
+        } catch (e) {
+            message.channel.send(`에러가 발생했습니다.\n\`${e}\``)
+        }
     }
 }
